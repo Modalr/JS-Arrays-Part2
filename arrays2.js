e806a2fdc7b2e2e0eaa8058ef9006711753c62e7
Moudhi AlRubaiaan <malrubaian@hotmail.com>
2024-11-03 17:17:28 +0300

    array

---
+++ arrays2.js
@@ -0,0 +1,47 @@
+function isArrayLengthOdd(numbers) {
+  const sizeOfArray = numbers.length;
+
+  if (sizeOfArray % 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isArrayLengthOdd([1, 2, 3, 4]));
+
+function isArrayLengthEven(numbers) {
+  if (numbers.length % 2) {
+    return false;
+  } else {
+    return true;
+  }
+}
+console.log(isArrayLengthEven([1, 2, 3, 4]));
+
+return isArrayLengthOdd(numbers);
+
+console.log(isArrayLengthEven([1, 2, 3, 4]));
+
+function addLailaToArray(instructors) {
+      copy array instructors
+
+    const newInstructors = instructors;
+    add "Laila " to array
+    newInstructors.push("Laila");
+    return new array 
+    return newInstructors;
+
+}
+ console.log(addLailaToArray(["Mishary","Hasan"])) 
+
+
+function eliminateTeam(teams) {
+    remove last element from teams
+ 
+   const teams.pop();
+    return teams;
+    return lastTeams;
+    
+}
+console.log(eliminateTeam(["Brazil","Germany","Italy"]));
